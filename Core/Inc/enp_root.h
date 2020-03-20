016ea710195798d0e5e55eb92eb3be3046918ef3
Oleg <o.pocheptsov@gmail.com>
2020-03-20 16:11:15 +0300

    FIX: enp_root & led

--- Core/Inc/enp_root.h
+++ Core/Inc/enp_root.h
@@ -3,24 +3,31 @@
 #include "enpapi.h"
 
 #define __usrdata __attribute__((section(".usr_const")))
-extern __usrdata ENP_Data_t DEVICE_UsrData;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // Identificators for nodes
 enum {
   NODE_ROOT,
   NODE_LED,
-  ENP_APP_NODENUM
+  ENP_NODENUM
 };
 
 // Identificators for variables
 enum ENP_APP_VAR {
-  ENP_APP_VAR_ERROR,  // error code
-  ENP_APP_VAR_DEV_ID, // device id
-  ENP_APP_VAR_TIME,   // up time
-  ENP_APP_VAR_NUM
+  ENP_ROOT_VAR_ERROR,  // error code
+  ENP_ROOT_VAR_DEV_ID, // device id
+  ENP_ROOT_VAR_TIME,   // up time
+  ENP_ROOT_VAR_NUM
 };
 
 // Atttributes of variables
-extern const ENP_Attr_t ICP_APP_VAR_ATTR[ENP_APP_VAR_NUM];
+extern const ENP_Attr_t ICP_APP_VAR_ATTR[ENP_ROOT_VAR_NUM];
 // Protocol init
 void ENP_DevInit();
+
+#ifdef __cplusplus
+}
+#endif
