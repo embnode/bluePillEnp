3fb8b6b94656e5f6660968b8e4d0e0900e9f46eb
Oleg <o.pocheptsov@gmail.com>
2020-03-20 15:07:05 +0300

    Changed Target name; Added new submodule address;

--- Core/Src/enp_usb.cpp
+++ Core/Src/enp_usb.cpp
@@ -1,38 +1,46 @@
-#include "enpapi.h"
-#include "usbd_cdc_if.h"
-#include "staticBuffersTmpl.h"
 #include "enp_usb.h"
+#include "enpapi.h"
+#include "staticBuffersTmpl.h"
+#include "usbd_cdc_if.h"
 
 // Object for enp protocol
 ENP_Handle_t hEnpUSB;
 
 staticBuffersTmpl::RingBuff<uint8_t, 256> usbBuff;
 
-void ENPUSB_readUsbBuff(uint8_t *buf, uint16_t len) {
-  int i = 0;
-  while (len) {
-    usbBuff.put(buf[i]);
-    i++;
-    len--;
-  }
+void ENPUSB_readUsbBuff(uint8_t* buf, uint16_t len)
+{
+    int i = 0;
+    while (len) {
+        usbBuff.put(buf[i]);
+        i++;
+        len--;
+    }
 }
 
 // Эта задача обслуживает обмен с конфигуратором
-void ENPUSB_Proc(void) { ENP_Proc(&hEnpUSB); }
-static int usb_getC(void) {
-  int ret = -1;
-  if (usbBuff.size()) {
-    ret = usbBuff.get();
-    usbBuff.pop();
-  }
-  return ret;
+void ENPUSB_Proc(void)
+{
+    ENP_Proc(&hEnpUSB);
 }
 
-static int usb_putN(const char *s, int n) {
-  CDC_Transmit_FS((uint8_t *)s, n);
-  return n;
+static int usb_getC(void)
+{
+    int ret = -1;
+    if (usbBuff.size()) {
+        ret = usbBuff.get();
+        usbBuff.pop();
+    }
+    return ret;
 }
 
-void ENPUSB_Init() {
-  ENP_InitHandle(&hEnpUSB, 17000, 17000, usb_getC, usb_putN);
+static int usb_putN(const char* s, int n)
+{
+    CDC_Transmit_FS((uint8_t*)s, n);
+    return n;
+}
+
+void ENPUSB_Init()
+{
+    ENP_InitHandle(&hEnpUSB, 1, 2, usb_getC, usb_putN);
 }
